3b6219feed1e5b11f53302fa78a8d8db1df5c6b8
WairimuThuku <91134225+WairimuThuku@users.noreply.github.com>
2021-11-15 03:32:08 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+let maleAkaNames = [
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi",
+  "Kwame",
+];
+
+let femaleAkanNames = [
+  "Akosua",
+  "Adwoa",
+  "Abenaa",
+  "Akua",
+  "Yea",
+  "Afua",
+  "Ama",
+];
+
+let daysofweek = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+
+function akangenerate(event) {
+  event.preventDefault();
+  let date = document.getElementById("date").value;
+  let gender = document.getElementById("gender").value;
+
+  let dayNumber = new Date(date).getDay();
+
+  if (gender == "" || date == "") {
+    alert("Error! You have to provide both your gender and date of birth");
+  } else if (gender == "Male") {
+    alert(`You were born on ${daysofweek[dayNumber]}
+            and your Akan name is ${maleAkaNames[dayNumber]}`);
+  } else {
+    alert(`You were born on ${daysofweek[dayNumber]}
+            and your Akan name is ${femaleAkanNames[dayNumber]}`);
+  }
+}
